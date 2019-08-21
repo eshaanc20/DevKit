c285812018f8ff768869703ed3df9ff834ef1759
Eshaan Chaudhari <eshaanchaudhari@hotmail.com>
2019-08-21 00:11:01 -0400

    Frontend for project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    response: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:9000/APIList')
+      .then(res => {
+        this.setState({
+          response: res.data
+        })
+      })
+      .catch(err => {
+        console.log(err);
+      })
+  }
+
+  render() {
+    return(
+      <div>
+        {this.state.response}
+      </div>
+    )
+  }
 }
 
 export default App;
