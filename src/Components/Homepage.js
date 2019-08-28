574f671da1de08c1697b3c89facb47a621eb0f8c
Anton Kanugalawattage <antondilon@gmail.com>
2019-08-28 06:17:37 -0400

    Updated homepage with image

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -2,6 +2,9 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import Search from './Search';
 import TopBar from './TopBar';
+import landingImg from './img/devkit-landing.png';
+import ReactTypingEffect from 'react-typing-effect';
+
 
 class Homepage extends Component{
   constructor(props){
@@ -32,6 +35,18 @@ class Homepage extends Component{
     return(
       <div>
         <TopBar/>
+        
+        <div style={{margin:'auto', flexWrap:'wrap',flexDirection: 'row' , display:'flex', maxWidth:'1200px'}}>
+         <div >
+        <h1 style={{fontSize:'190px', fontFamily:'Avenir', fontWeight:'900', height:'200px'}}>DevKit.</h1>
+        <h3 style={{fontSize:'25px', fontFamily:'HelveticaNeue', fontWeight:'500', paddingTop:'10px'}} >Search for your favourite Software Tools with</h3>
+        <h3 style={{fontSize:'25px', fontFamily:'HelveticaNeue', fontWeight:'500', paddingTop:'0px'}}>over 1000 APIs</h3>
+         </div>
+         <div style={{maxWidth:'500px'}}>
+        <img src={landingImg} style={{maxWidth:'500px'}} alt=""></img>
+         </div>
+        </div>
+
         {this.APIList!=null? <Search cardInfos={this.APIList} />:null}
       </div>
     )
